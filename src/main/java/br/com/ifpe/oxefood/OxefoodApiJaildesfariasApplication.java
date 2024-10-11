f2c84067403a55e70942c6fca1360bdcb9fc2236
luno@.ifpe <aluno@ifpe.com>
2024-10-10 21:26:21 -0300

    primeiro commmit

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiJaildesfariasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJaildesfariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJaildesfariasApplication.class, args);
+	}
+
+}
