fbc7a5eb79584bf7ab9da9ddc2c66dc1cf874103
luno@.ifpe <aluno@ifpe.com>
2024-11-14 21:17:54 -0300

    Implementando a Inclusão no Back-end

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 import br.com.ifpe.oxefood.api.cliente.ClienteRequest;
@@ -29,6 +30,16 @@ public class ClienteController {
        
     
        return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+   }
+
+   @GetMapping
+   public List<Cliente> listarTodos() {
+       return clienteService.listarTodos();
+   }
+
+   @GetMapping("/{id}")
+   public Cliente obterPorID(@PathVariable Long id) {
+       return clienteService.obterPorID(id);
    }
 }
 
--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
 import jakarta.transaction.Transactional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,17 @@ public class ClienteService {
        cliente.setHabilitado(Boolean.TRUE);
        return repository.save(cliente);
    }
+       
+   public List<Cliente> listarTodos() {
+        
+    return repository.findAll();//select*from
+}
 
+public Cliente obterPorID(Long id) {
+
+    return repository.findById(id).get();//select*from cliente where id =?
+}
+
+        
 }
 
