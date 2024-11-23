e2f3e2de3114c00dac5bae34113c96e485df0b99
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-11-22 22:19:34 -0300

    Update EntregadorRequest.java

--- src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorRequest.java
@@ -22,13 +22,9 @@ public class EntregadorRequest {
    
    @JsonFormat(pattern = "dd/MM/yyyy")
    private LocalDate dataNascimento;
-
    private String foneCelular;
-
    private String foneFixo;
-
    private Integer qtdEntregasRealizadas;
-
    private Double valorFrete;
    private String enderecoRua;
    private String enderecoCompleto;
@@ -38,16 +34,26 @@ public class EntregadorRequest {
    private String enderecoCep;
    private String enderecoUf;
    private Boolean ativo;
-
    public Entregador build() {
 
     return Entregador.builder()
-        .nome(nome)
-        .dataNascimento(dataNascimento)
-        .cpf(cpf)
-        .foneCelular(foneCelular)
-        .foneFixo(foneFixo)
-        .build();
+              .nome(nome)
+              .dataNascimento(dataNascimento)
+              .cpf(cpf)
+              .foneCelular(foneCelular)
+              .foneFixo(foneFixo)
+              .qtdEntregasRealizadas(qtdEntregasRealizadas)
+              .valorFrete(valorFrete)
+              .enderecoRua(enderecoRua)
+              .enderecoCompleto(enderecoCompleto)
+              .enderecoNumero(enderecoNumero)
+              .enderecoBairro(enderecoBairro)
+              .enderecoCidade(enderecoCidade)
+              .enderecoCep(enderecoCep)
+              .enderecoUf(enderecoUf)
+              .ativo(ativo)
+              .build();
+              
 }
 
 }
