b6728991738e56c90d010bce2d8f874e322c8df1
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-11-24 16:46:24 -0300

    Update ClienteController.java

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -11,45 +11,41 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
+
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 import br.com.ifpe.oxefood.api.cliente.ClienteRequest;
 
-@RestController
-@RequestMapping("/api/cliente")
-@CrossOrigin
+@RestController //Define a classe como um controlador REST.
+@RequestMapping("/api/cliente")//Mapeia o endpoint base (/api/cliente).
+@CrossOrigin// Permite chamadas de diferentes origens (CORS).
 public class ClienteController {
 
-   @Autowired
-   private ClienteService clienteService;
+    @Autowired
+    private ClienteService clienteService;
 
-   @PostMapping
-   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+    @PostMapping //Mapear rotas HTTP específicas (POST, GET, PUT).
+    public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {//ResponseEntity:Encapsula as respostas HTTP com status e corpo.
 
-     
-       Cliente cliente = clienteService.save(request.build());
-       
-    
-       return new ResponseEntity<>(cliente, HttpStatus.CREATED);
-   }
+        Cliente cliente = clienteService.save(request.build());
+        return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+    }
 
-   @GetMapping
-   public List<Cliente> listarTodos() {
-       return clienteService.listarTodos();
-   }
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
 
-   @GetMapping("/{id}")
-   public Cliente obterPorID(@PathVariable Long id) {
-       return clienteService.obterPorID(id);
-       
-   }
-   
- @PutMapping("/{id}")
- public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
+    @GetMapping("/{id}")
+    public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
 
-       clienteService.update(id, request.build());
-       return ResponseEntity.ok().build();
- }
-
+    @PutMapping("/{id}")//Extrai o valor do ID da URL
+    public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
+        clienteService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
 }
