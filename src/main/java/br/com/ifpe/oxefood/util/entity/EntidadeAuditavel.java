a03a256de0098b29f70faa086645589997950ac5
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-11-24 21:10:42 -0300

    Update EntidadeAuditavel.java

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
@@ -5,7 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import jakarta.persistence.Id;
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+
 import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import jakarta.persistence.Version;
