e8569a3430946b1765dc7a3949c9a3543de9c12f
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-11-27 14:45:50 -0300

    Update EntregadorRepository.java

--- src/main/java/br/com/ifpe/oxefood/modelo/Entregador/EntregadorRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/Entregador/EntregadorRepository.java
@@ -1,1 +1,12 @@
 
+package br.com.ifpe.oxefood.modelo.Entregador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregadoRepository extends JpaRepository<.Entregado, Long> {
+        
+    public List<Entregado> findByNome(String nome);//  buscar Entregado pelo nome
+
+    public Entregado findByCpf(String cpf);//buscar .Entregado pelo CPF
+  
+}
