b9106ac52c7a4a285b85c1689f358158e066f991
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-01 21:13:27 -0300

    Update EntregadorController.java

--- src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorController.java
@@ -4,54 +4,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
-import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/api/entregador")
 @CrossOrigin
 public class EntregadorController {
 
-   @Autowired
-   private EntregadorService entregadorService;
+    @Autowired
+    private EntregadorService entregadorService;
 
-   @PostMapping
-   public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
-       // Usar o método save do entregadorService
-       Entregador entregador = entregadorService.save(request.build());
-       return new ResponseEntity<>(entregador, HttpStatus.CREATED);
-   }
+    @PostMapping
+    public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
+        Entregador entregador = entregadorService.save(request.build());
+        return ResponseEntity.status(HttpStatus.CREATED).body(entregador);
+    }
 
-   @Transactional
-   public void update(Long id, Entregador entregadorAlterado) {
-       
-       Entregador entregador = entregadorService.findById(id)// Buscar o entregador pelo ID
-               .orElseThrow(() -> new RuntimeException("Entregador não encontrado"));
+    @PutMapping("/{id}")
+    public ResponseEntity<Entregador> update(@PathVariable Long id, @RequestBody EntregadorRequest request) {
+        Entregador entregadorAlterado = request.build();
 
-       entregador.setNome(entregadorAlterado.getNome()); // Atualizar os campos do entregador
-       entregador.setCpf(entregadorAlterado.getCpf());
-       entregador.setRg(entregadorAlterado.getRg());
-       entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
-       entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
-       entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
-       entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
-       entregador.setValorFrete(entregadorAlterado.getValorFrete());
-       entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
-       entregador.setEnderecoCompleto(entregadorAlterado.getEnderecoCompleto());
-       entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
-       entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
-       entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
-       entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
-       entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());c
-       entregador.setAtivo(entregadorAlterado.getAtivo());
+        Entregador entregador = entregadorService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Entregador não encontrado"));
 
-       
-       entregadorService.update(entregador);//Salvar mudanças
-   }
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setRg(entregadorAlterado.getRg());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+        entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+        entregador.setValorFrete(entregadorAlterado.getValorFrete());
+        entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+        entregador.setEnderecoCompleto(entregadorAlterado.getEnderecoCompleto());
+        entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
+        entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+        entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+        entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+        entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
+        entregador.setAtivo(entregadorAlterado.getAtivo());
+
+        entregadorService.update(entregador);
+        return ResponseEntity.ok(entregador);
+    }
 }
