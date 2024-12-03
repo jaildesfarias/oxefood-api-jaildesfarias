e9403e1dde04e588ae745e226f04a8f913d0bb7e
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-03 19:25:15 -0300

    Update VendaReposirtory.java

--- src/main/java/br/com/ifpe/oxefood/modelo/venda/VendaReposirtory.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/venda/VendaReposirtory.java
@@ -1,1 +1,6 @@
+package br.com.ifpe.oxefood.modelo.venda;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Venda, Long> {
+}
