317d461a1ad9fb100ab9cb059f5534aafee9e4a0
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-07 19:24:49 -0300

    Update ProdutoController.java

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -9,23 +9,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
-import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
+import br.com.ifpe.oxefood.modelo.categoria.CategoriaProdutoService;
 
 @RestController
 @RequestMapping("/api/produto")
 @CrossOrigin
 public class ProdutoController {
 
-   @Autowired
-   private ProdutoService produto Service;
+    @Autowired
+    private ProdutoService produtoService;
 
-   @PostMapping
-   public ResponseEntity<Produto> save(@RequestBody ProdutoRequest request) {
+    @Autowired
+    private CategoriaProdutoService categoriaProdutoService;
 
-       Produto produto = ProdutoService.save(request.build());
-       return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
-   }
+    @PostMapping
+    public ResponseEntity<Produto> save(@RequestBody ProdutoRequest request) {
+        // Constrói o objeto Produto a partir do ProdutoRequest
+        Produto produtoNovo = request.build();
+        
+        // Define a categoria do produto, recuperando pelo ID da categoria no request
+        produtoNovo.setCategoria(categoriaProdutoService.obterPorID(request.getIdCategoria()));
+        
+        // Salva o produto no serviço
+        Produto produtoSalvo = produtoService.save(produtoNovo);
+        
+        // Retorna o produto salvo com status 201 CREATED
+        return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
+    }
 }
-
-
