7972c4c047279d136daa788d62ec77643ddd7265
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2024-12-09 22:18:26 -0300

    Update ProdutoException.java
    Você pode lançar essa exceção quando encontrar algum erro no valor mínimo do produto ou outra lógica relacionada. Por exemplo, no seu ProdutoService

--- src/main/java/br/com/ifpe/oxefood/util/entity/exception/ProdutoException.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/exception/ProdutoException.java
@@ -1,12 +1,16 @@
 package br.com.ifpe.oxefood.util.entity.exception;
 
-@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+import org.springframework.http.HttpStatus; 
+import org.springframework.web.bind.annotation.ResponseStatus; 
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR) // Define o status HTTP da exceção
 public class ProdutoException extends RuntimeException {
 
-    public static final String MSG_VALOR_MINIMO_PRODUTO = "Não é permitido inserir produtos com valores inferiores a R$ 10.";
+    public static final String MSG_VALOR_MINIMO_PRODUTO = 
+        "Não é permitido inserir produtos com valores inferiores a R$ 10.";
 
+    // Construtor da exceção
     public ProdutoException(String msg) {
-
-	super(String.format(msg));
+        super(msg); // Usa a mensagem passada como parâmetro
     }
 }
