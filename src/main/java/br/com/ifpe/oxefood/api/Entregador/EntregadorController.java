547f9ca4bcf48f2066382a4d51ca201c86abd99c
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-01-25 22:43:01 -0300

    Update EntregadorController.java

--- src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/Entregador/EntregadorController.java
@@ -1,19 +1,5 @@
 package br.com.ifpe.oxefood.api.entregador;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.ifpe.oxefood.modelo.entregador.Entregador;
-import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
-
 @RestController
 @RequestMapping("/api/entregador")
 @CrossOrigin
@@ -35,6 +21,7 @@ public class EntregadorController {
         Entregador entregador = entregadorService.findById(id)
                 .orElseThrow(() -> new RuntimeException("Entregador não encontrado"));
 
+        // Atualiza os dados do entregador
         entregador.setNome(entregadorAlterado.getNome());
         entregador.setCpf(entregadorAlterado.getCpf());
         entregador.setRg(entregadorAlterado.getRg());
@@ -52,8 +39,8 @@ public class EntregadorController {
         entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
         entregador.setAtivo(entregadorAlterado.getAtivo());
 
+        // Salva a atualização
         entregadorService.update(entregador);
         return ResponseEntity.ok(entregador);
     }
 }
-//erro  entregadorService.update(entregador);obseva
